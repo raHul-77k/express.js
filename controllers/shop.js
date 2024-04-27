bdde3c933ba72e6ceb98ed041a2cd043d1ea6ec9
Rahul Kumar <rrahul772k@gmail.com>
2024-04-27 13:48:22 +0530

    adding cart quantity

--- controllers/shop.js
+++ controllers/shop.js
@@ -78,6 +78,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   let fetchedCart
+  let newQuantity = 1;
   req.user
   .getCart()
   .then(cart=>{
@@ -89,18 +90,20 @@ exports.postCart = (req, res, next) => {
     if(products.length>0){
       product = products[0];
     }
-    let newQuantity = 1;
-    if(product){
 
+    if(product){
+      const oldQuantity = product.cartItem.quantity;
+      newQuantity = oldQuantity+1;
+      return product;
     }
     return Product.findById(prodId)
-    .then(product => {
-      return fetchedCart.addProduct(product,{
-        through: { quanity:newQuantity }
-      });
-    })
     .catch(err=>{
       console.log(err);
+    });
+  })
+  .then(product=>{
+    return fetchedCart.addProduct(product,{
+      through: { quanity:newQuantity }
     });
   })
   .then(()=>{
